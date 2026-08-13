de5dae7d2cd61358c4a758eb7a38ef665c191e69
kangrisuk-id <kangrisuk@gmail.com>
2026-08-13 22:13:12 +0700

    Change fallbackToDestructiveMigration to drop all tables

--- app/src/main/java/com/laskarfkapp/data/AppDatabase.kt
+++ app/src/main/java/com/laskarfkapp/data/AppDatabase.kt
@@ -36,7 +36,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "zakat_hutang_db"
-                ).fallbackToDestructiveMigration().build()
+                ).fallbackToDestructiveMigration(dropAllTables = true).build()
                 INSTANCE = instance
                 instance
             }
